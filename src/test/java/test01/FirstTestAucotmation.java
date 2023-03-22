b5cf8d846c5c3b8c38ea469c77796a98824d1bb2
Abu Bakar <siddique0153@gmail.com>
2023-03-22 16:41:46 +0600

    test

---
+++ src/test/java/test01/FirstTestAucotmation.java
@@ -0,0 +1,24 @@
+package test01;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTestAucotmation {
+	
+	
+  @Test
+  public void testGoogle() {
+	WebDriver driver;
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("headless");
+    options.addArguments("disable-gpu");
+    driver = new ChromeDriver(options);
+    driver.get("https://www.google.com");
+    System.out.println("Title of the page is: " + driver.getTitle());
+    Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+   }
+}
