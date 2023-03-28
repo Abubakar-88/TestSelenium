42847ddff74686843d3f2a989d7372706a1e02ba
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-03-28 12:19:56 +0600

    Update FirstTestAucotmation.java

--- src/test/java/test01/FirstTestAucotmation.java
+++ src/test/java/test01/FirstTestAucotmation.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 @Test
 public class FirstTestAucotmation {
 	static WebDriver driver;
-	public static void main(String[] args) {
+	public void OpenBrowser {
 	    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	      
 	      // Set Chrome options
